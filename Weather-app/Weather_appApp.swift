391f6261cb2b6e017af5aa44d316de0e314305d3
MuhammadAhsan-Shafiq <ma5639176@gmail.com>
2024-07-27 15:45:03 +0500

    Initial Commit

---
+++ Weather-app/Weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_appApp.swift
+//  Weather-app
+//
+//  Created by MacBook Pro on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
